6441c2b71f526bc6d06978c688ab34a45aa0d6ab
24amaris <147129858+24amaris@users.noreply.github.com>
2024-04-02 23:44:29 +0300

    Add files via upload

---
+++ AvionDeLupta.java
@@ -0,0 +1,10 @@
+
+public class AvionDeLupta extends Avion {
+    public AvionDeLupta(String planeID) {
+        super(planeID);
+    }
+
+    public void launchMissile() {
+        System.out.println(getPlaneID() + " - Launching missile");
+    }
+}
